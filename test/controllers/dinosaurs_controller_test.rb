3a1a41d1aaf4fb1a6e19ddbdbe06fe8c0154a8ff
Greg Winn <winn.greg@gmail.com>
2023-09-01 12:04:58 -0500

    tests: Added dino tests for model and controller

--- test/controllers/dinosaurs_controller_test.rb
+++ test/controllers/dinosaurs_controller_test.rb
@@ -1,23 +1,55 @@
 require "test_helper"
 
 class DinosaursControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @user = users(:user_one)
+    @dinosaur = dinosaurs(:dinosaur_one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get dinosaurs_index_url
+    get dinosaurs_path
     assert_response :success
+    assert_not_nil assigns(:dinosaurs)
   end
 
   test "should get new" do
-    get dinosaurs_new_url
+    get new_dinosaur_path
+    assert_response :success
+  end
+
+  test "should create dinosaur" do
+    assert_difference("Dinosaur.count") do
+      post dinosaurs_path, params: { dinosaur: { name: "Rexy", species: "Tyrannosaurus", cage_id: @dinosaur.cage_id } }
+    end
+
+    assert_redirected_to dinosaurs_path
+    assert_equal "Roarrr! A new dino has hatched and is now comfortably chilling in its cage.", flash[:notice]
+  end
+
+  test "should show dinosaur" do
+    get dinosaur_path(@dinosaur)
     assert_response :success
   end
 
   test "should get edit" do
-    get dinosaurs_edit_url
+    get edit_dinosaur_path(@dinosaur)
     assert_response :success
   end
 
-  test "should get show" do
-    get dinosaurs_show_url
-    assert_response :success
+  test "should update dinosaur" do
+    patch dinosaur_path(@dinosaur), params: { dinosaur: { name: "Updated Name" } }
+    assert_redirected_to dinosaurs_path
+    assert_equal "Roarrr! The dino has been hatched... again.", flash[:notice]
+  end
+
+  test "should destroy dinosaur" do
+    assert_difference("Dinosaur.count", -1) do
+      delete dinosaur_path(@dinosaur)
+    end
+
+    assert_redirected_to dinosaurs_path
+    assert_equal "Well, my dear friend, deleting a dinosaur is like erasing a footnote in history.", flash[:notice]
   end
 end
--- test/models/dinosaur_test.rb
+++ test/models/dinosaur_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class DinosaurTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save valid dinosaur" do
+    cage = cages(:cage_with_space)
+    dinosaur = Dinosaur.new(name: "Rexy", species: 0, cage_id: cage.id)
+    assert dinosaur.save, "Could not save valid dinosaur"
+  end
+
+  test "should not save dinosaur without a name" do
+    dinosaur = Dinosaur.new(species: "Tyrannosaurus")
+    assert_not dinosaur.save, "Saved dinosaur without a name"
+  end
+
+  test "should not save dinosaur without species" do
+    dinosaur = Dinosaur.new(name: "Rexy", cage_id: 1)
+    assert_not dinosaur.save, "Saved dinosaur without species"
+  end
+
+  test "should not save dinosaur with invalid species" do
+    dinosaur = Dinosaur.new(name: "Rexy", species: 2, cage_id: 1)
+    assert_not dinosaur.save, "Saved dinosaur with invalid species"
+  end
 end
